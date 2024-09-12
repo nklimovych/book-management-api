18532c62c50b63bf6f141a18abf66298e105bd5c
Nazar Klimovych <rksvjdbx@gmail.com>
2024-09-12 11:08:24 +0300

    Add GlobalExceptionHandler and create custom EntityNotFoundException

---
+++ src/main/java/com/book/management/exception/EntityNotFoundException.java
@@ -0,0 +1,7 @@
+package com.book.management.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
